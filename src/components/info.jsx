6ac2160a1f58dbd7c54af2bd4620177e34a099cf
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-17 22:26:38 +0100

    Made the Email button functional

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -11,9 +11,11 @@ const Body = () => {
         <p className='job'>Frontend Developer</p>
         <a className='site'href='www.adeboluwarinthomas.netlify.app'>Adeboluwarinthomas.website</a>
         <div className='btn'>
+        <a href="mailto:adeboluwarinthomas@gmail.com?subject=Contact mail&body=Hi Adeboluwarin,I would like to">
         <button>
           <img src={Mail} className='mail-img'alt='Mail'/> Email
         </button>
+        </a>
         </div>
       </div>
     </div>
