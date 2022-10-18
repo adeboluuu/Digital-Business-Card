70ce2a3f67f11b1c941f9d5361d51519c34d833a
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-18 23:54:13 +0100

    Changed the image,adjusted the margins for the about and interest section

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from '../assets/image.jpg'
+import Image from '../assets/image.png'
 import Mail from '../assets/Mail.svg'
 
 const Body = () => {
@@ -9,7 +9,7 @@ const Body = () => {
       <div className='info'>
         <p className='name'>Adeboluwarin Thomas</p>
         <p className='job'>Frontend Developer</p>
-        <a className='site'href='www.adeboluwarinthomas.netlify.app'>Adeboluwarinthomas.website</a>
+        <a className='site'href='www.adeboluwarinthomas.netlify.app'>Portfolio</a>
         <div className='btn'>
         <a href="mailto:adeboluwarinthomas@gmail.com?subject=Contact mail&body=Hi Adeboluwarin,I would like to">
         <button>
